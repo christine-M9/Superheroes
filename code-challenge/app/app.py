1cc7c9601859c61445d19f890dc1639e48d3f3e4
Christine <christinekhekama99@gmail.com>
2023-10-02 01:06:48 +0300

    adding the GET methods

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
-
 from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
@@ -15,6 +14,52 @@ db.init_app(app)
 def home():
     return ''
 
+## Routes
+
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes = Hero.query.all()
+    hero_list = [{'id': hero.id, 'name': hero.name, 'super_name': hero.super_name} for hero in heroes]
+    return jsonify(hero_list)
+
+@app.route('/heroes/<int:id>', methods=['GET'])
+def get_hero(id):
+    hero = Hero.query.get(id)
+    if not hero:
+        return make_response(jsonify({'error': 'Hero not found'}), 404)
+
+    powers = [{'id': power.id, 'name': power.name, 'description': power.description} for power in hero.powers]
+
+    hero_data = {
+        'id': hero.id,
+        'name': hero.name,
+        'super_name': hero.super_name,
+        'powers': powers
+    }
+
+    return jsonify(hero_data)
+
+@app.route('/powers', methods=['GET'])
+def get_powers():
+    powers = Power.query.all()
+    power_list = [{'id': power.id, 'name': power.name, 'description': power.description} for power in powers]
+    return jsonify(power_list)
+
+@app.route('/powers/<int:id>', methods=['GET'])
+def get_power(id):
+    power = Power.query.get(id)
+    if not power:
+        return make_response(jsonify({'error': 'Power not found'}), 404)
+
+    power_data = {
+        'id': power.id,
+        'name': power.name,
+        'description': power.description
+    }
+
+    return jsonify(power_data)
+
 if __name__ == '__main__':
     app.run(port=5555)
+
 
