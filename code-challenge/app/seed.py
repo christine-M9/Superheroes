2e94ff73e64e6aea009dd6c7cf0892774ce4460c
Christine <christinekhekama99@gmail.com>
2023-10-02 00:45:54 +0300

    seeding

--- code-challenge/app/seed.py
+++ code-challenge/app/seed.py
@@ -1,35 +1,39 @@
-puts "🦸‍♀️ Seeding powers..."
-Power.create([
-  { name: "super strength", description: "gives the wielder super-human strengths" },
-  { name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" },
-  { name: "super human senses", description: "allows the wielder to use her senses at a super-human level" },
-  { name: "elasticity", description: "can stretch the human body to extreme lengths" }
-])
+from faker import Faker
+from models import db, Hero, Power, HeroPower
+from flask import Flask
+from sqlalchemy import func
 
-puts "🦸‍♀️ Seeding heroes..."
-Hero.create([
-  { name: "Kamala Khan", super_name: "Ms. Marvel" },
-  { name: "Doreen Green", super_name: "Squirrel Girl" },
-  { name: "Gwen Stacy", super_name: "Spider-Gwen" },
-  { name: "Janet Van Dyne", super_name: "The Wasp" },
-  { name: "Wanda Maximoff", super_name: "Scarlet Witch" },
-  { name: "Carol Danvers", super_name: "Captain Marvel" },
-  { name: "Jean Grey", super_name: "Dark Phoenix" },
-  { name: "Ororo Munroe", super_name: "Storm" },
-  { name: "Kitty Pryde", super_name: "Shadowcat" },
-  { name: "Elektra Natchios", super_name: "Elektra" }
-])
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-puts "🦸‍♀️ Adding powers to heroes..."
+# Create an application context
+with app.app_context():
+    fake = Faker()
 
-strengths = ["Strong", "Weak", "Average"]
-Hero.all.each do |hero|
-  rand(1..3).times do
-    # get a random power
-    power = Power.find(Power.pluck(:id).sample)
+    # Ensure we have some powers available
+    if Power.query.count() == 0:
+        default_powers = [
+            Power(name="Super Strength", description="Gives incredible physical strength."),
+            Power(name="Teleportation", description="Can instantly move from one place to another."),
+            Power(name="Invisibility", description="Can become invisible at will.")
+        ]
+        db.session.add_all(default_powers)
+        db.session.commit()
 
-    HeroPower.create!(hero_id: hero.id, power_id: power.id, strength: strengths.sample)
-  end
-end
+    for _ in range(10):  # Generate 10 heroes
+        hero = Hero(name=fake.unique.first_name(), super_name=fake.unique.first_name())
+        db.session.add(hero)
+        for _ in range(fake.random_int(1, 3)):
+            power = Power.query.order_by(func.random()).first()  # Get a random power
+            strength = fake.random_element(["Strong", "Weak", "Average"])
+            hero_power = HeroPower(hero=hero, power=power, strength=strength)
+            db.session.add(hero_power)
 
-puts "🦸‍♀️ Done seeding!"
+    db.session.commit()
+    print("🦸‍♀️ Done seeding!")
+
+
+
+
